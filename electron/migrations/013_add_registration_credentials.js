fa5d3ec9a168a552fa36264ba4f800d5f0045609
anupriyaclimeto-beep <anupriya.climeto@gmail.com>
2026-08-14 11:14:03 +0530

    Remove redundant local migration

--- electron/migrations/013_add_registration_credentials.js
+++
@@ -1,6 +0,0 @@
-export const up = `
-  ALTER TABLE registration_details ADD COLUMN email TEXT;
-  ALTER TABLE registration_details ADD COLUMN mobile TEXT;
-  ALTER TABLE registration_details ADD COLUMN password TEXT;
-  ALTER TABLE registration_details ADD COLUMN confirm_password TEXT;
-`;
